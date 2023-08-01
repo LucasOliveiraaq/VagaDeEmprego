98b7d52a676ab90add04cb667a51dbeb02e22224
Lucas P Oliveira <lucasp.oliveira122@gmail.com>
2023-08-01 06:48:44 -0300

    integrando com a api e add o handlebars

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const exphbs = require('express-handlebars');
 const app = express();
+const path = require('path');
 const db = require('./db/connection')
 const bodyParser = require('body-parser');
+const job = require('./models/Job');
+const Job = require('./models/Job');
+const sequelize = require('sequelize');
+const op = sequelize.Op;
 
 const PORT = 3000;
 
@@ -12,6 +18,14 @@ app.listen(PORT, function(){
 //body parser 
 app.use(bodyParser.urlencoded({extended : false}));
 
+//handle bars 
+app.set('views', path.join(__dirname, 'views'));
+app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+//static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
 //db connection
 db.authenticate().then(() => {
     console.log("Conectou ao banco com sucesso");
@@ -21,7 +35,26 @@ db.authenticate().then(() => {
 
 //routes
 app.get("/", (req, res) => {
-    res.send("Está funcionando 3");
+    
+    let search = req.query.job;
+    let query = '%'+ search + '%';
+
+    if(!search){
+        job.findAll({
+            order: [['new_job', 'DESC']]
+        })
+        .then(jobs => {
+            res.render('index', {jobs});        
+        });
+    } else { //Quando estiver buscar
+        job.findAll({
+            where: {title: {[op.iLike]:query}},
+            order: [['new_job', 'DESC']]
+        })
+        .then(jobs => {
+            res.render('index', {jobs, search});        
+        });
+    }
 });
 
 //Jobs routes
