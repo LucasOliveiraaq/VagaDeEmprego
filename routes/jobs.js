cded9395dcf284ebbccb8f83fe0f849b8d26c4f1
Lucas P Oliveira <lucasp.oliveira122@gmail.com>
2023-08-02 07:01:44 -0300

    view ver vaga

--- routes/jobs.js
+++ routes/jobs.js
@@ -7,6 +7,19 @@ router.get('/test', (req, res) =>{
     res.send('deu certo');
 });
 
+//detalhe da vaga
+router.get('/view/:id', (req, res) => job.findOne({
+    where: {id: req.params.id}
+  }).then(job => {
+  
+    res.render('view', {
+      job
+    });
+  
+  }).catch(err => console.log(err)));
+  
+
+//roda de envio
 router.get('/add', (req, res) =>{
     res.render('add');
 });
